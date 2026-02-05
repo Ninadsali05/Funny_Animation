0cd33b0a77342b19d95161c8208702059f119ced
Ninadsali05 <ninad2388@gmail.com>
2026-02-05 17:26:47 +0530

    Implement petal animation on button click
    Add petal animation effect to the interface.

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
   document.querySelector(".buttons").style.display = "none";
   result.classList.remove("hidden");
+
+  startPetals();
 });
+
+function startPetals() {
+  const petalsContainer = document.getElementById("petals");
+
+  setInterval(() => {
+    const petal = document.createElement("div");
+    petal.classList.add("petal");
+    petal.innerText = "🌸";
+
+    petal.style.left = Math.random() * 100 + "vw";
+    petal.style.animationDuration = (Math.random() * 3 + 3) + "s";
+
+    petalsContainer.appendChild(petal);
+
+    setTimeout(() => {
+      petal.remove();
+    }, 6000);
+  }, 300);
+}
+
